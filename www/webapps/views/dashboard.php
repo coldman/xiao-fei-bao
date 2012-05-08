242bde6bddc03016f477bf219a96aa1e5f138cd6
zxk0610@gmail.com <zxk0610@gmail.com@d3b773b6-36a7-1bf0-39a4-6fa0203e7e18>
2012-05-08 15:29:30 +0000

    update

--- www/webapps/views/dashboard.php
+++ www/webapps/views/dashboard.php
@@ -1,88 +1,171 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1"><title>
+	欢迎使用 ligerUI 权限管理系统
+</title><link href="lib/ligerUI/skins/Aqua/css/ligerui-all.css" rel="stylesheet" type="text/css" /><link href="lib/ligerUI/skins/Gray/css/all.css" rel="stylesheet" type="text/css" />  
+    <script src="lib/jquery/jquery-1.5.2.min.js" type="text/javascript"></script>
+    <script src="lib/ligerUI/js/ligerui.min.js" type="text/javascript"></script>  
+    <script src="lib/ligerUI/js/plugins/ligerTab.js" type="text/javascript"></script>
+    <script src="lib/ligerUI/js/plugins/ligerLayout.js" type="text/javascript"></script>
+    <link href="lib/css/common.css" rel="stylesheet" type="text/css" /><link href="lib/css/index.css" rel="stylesheet" type="text/css" />
 
-	<style type="text/css">
+    <script src="lib/js/common.js" type="text/javascript"></script>
+    <script src="lib/js/LG.js" type="text/javascript"></script>
+    <script src="lib/js/login.js" type="text/javascript"></script>
 
-	::selection{ background-color: #E13300; color: white; }
-	::moz-selection{ background-color: #E13300; color: white; }
-	::webkit-selection{ background-color: #E13300; color: white; }
+        <script src="../lib/jquery-validation/jquery.validate.min.js" type="text/javascript"></script> 
+    <script src="../lib/jquery-validation/jquery.metadata.js" type="text/javascript"></script>
+    <script src="../lib/jquery-validation/messages_cn.js" type="text/javascript"></script>
 
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body{
-		margin: 0 15px 0 15px;
-	}
-	
-	p.footer{
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-	
-	#container{
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		-webkit-box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+    <script src="lib/js/changepassword.js" type="text/javascript"></script>
+    <script src="lib/ligerUI/js/plugins/ligerForm.js" type="text/javascript"></script>
 </head>
-<body>
+<body style="text-align:center; background:#F0F0F0; overflow:hidden;">
+    <div id="pageloading" style="display:block;"></div> 
+    <div id="topmenu" class="l-topmenu">
+        <div class="l-topmenu-logo">权限管理系统 LigerRightManager</div>
+        <div class="l-topmenu-welcome"> 
+            <span class="l-topmenu-username"></span>欢迎您  &nbsp; 
+            [<a href="javascript:f_changepassword()">修改密码</a>] &nbsp; 
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+             [<a href="javascript:f_login()">切换用户</a>]
+            [<a href="login.htm?Action=out">退出</a>] 
+        </div> 
+        
+    </div> 
+     <div id="mainbody" class="l-mainbody" style="width:99.2%; margin:0 auto; margin-top:3px;" >
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+        <div position="left" title="主要菜单" id="mainmenu"></div>  
+        <div position="center" id="framecenter"> 
+            <div tabid="home" title="我的主页"> 
+                <iframe frameborder="0" name="home" id="home" src="welcome.aspx"></iframe>
+            </div> 
+        </div> 
+    </div>
+    <div class="l-hidden"><script src="http://s24.cnzz.com/stat.php?id=3923823&web_id=3923823" language="JavaScript"></script></div>
+    <script type="text/javascript">
+        //几个布局的对象
+        var layout, tab, accordion;
+        //tabid计数器，保证tabid不会重复
+        var tabidcounter = 0;
+        //窗口改变时的处理函数
+        function f_heightChanged(options) {
+            if (tab)
+                tab.addHeight(options.diff);
+            if (accordion && options.middleHeight - 24 > 0)
+                accordion.setHeight(options.middleHeight - 24);
+        }
+        //增加tab项的函数
+        function f_addTab(tabid, text, url) {
+            if (!tab) return;
+            if (!tabid)
+            {
+                tabidcounter++;
+                tabid = "tabid" + tabidcounter; 
+            }
+            tab.addTabItem({ tabid: tabid, text: text, url: url });
+        }
+        //登录
+        function f_login()
+        {
+            LG.login();
+        }
+        //修改密码
+        function f_changepassword()
+        {
+            LG.changepassword();
+        }
+        $(document).ready(function ()
+        {
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+            //菜单初始化
+            $("ul.menulist li").live('click', function ()
+            {
+                var jitem = $(this);
+                var tabid = jitem.attr("tabid");
+                var url = jitem.attr("url");
+                if (!url) return;
+                if (!tabid)
+                {
+                    tabidcounter++;
+                    tabid = "tabid" + tabidcounter;
+                    jitem.attr("tabid", tabid);
 
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
+                    //给url附加menuno
+                    if (url.indexOf('?') > -1) url += "&";
+                    else url += "?";
+                    url += "MenuNo=" + jitem.attr("menuno");
+                    jitem.attr("url", url);
+                }
+                f_addTab(tabid, $("span:first", jitem).html(), url);
+            }).live('mouseover', function ()
+            {
+                var jitem = $(this);
+                jitem.addClass("over");
+            }).live('mouseout', function ()
+            {
+                var jitem = $(this);
+                jitem.removeClass("over");
+            });
 
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
+            //布局初始化 
+            //layout
+            layout = $("#mainbody").ligerLayout({ height: '100%', heightDiff: -3, leftWidth: 140, onHeightChanged: f_heightChanged, minLeftWidth: 120 });
+            var bodyHeight = $(".l-layout-center:first").height();
+            //Tab
+            tab = $("#framecenter").ligerTab({ height: bodyHeight, contextmenu: true });
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
-</div>
 
+            //预加载dialog的背景图片
+            LG.prevDialogImage();
+
+            var mainmenu = $("#mainmenu");
+
+            $.getJSON('handler/tree.ashx?view=MyMenus&rnd='+Math.random(), function (menus)
+            {
+                $(menus).each(function (i, menu)
+                {
+                    var item = $('<div title="' + menu.MenuName + '"><ul class="menulist"></ul></div>');
+
+                    $(menu.children).each(function (j, submenu)
+                    {
+                        var subitem = $('<li><img/><span></span><div class="menuitem-l"></div><div class="menuitem-r"></div></li>');
+                        subitem.attr({
+                            url: submenu.MenuUrl,
+                            menuno: submenu.MenuNo
+                        });
+                        $("img", subitem).attr("src", submenu.MenuIcon || submenu.icon);
+                        $("span", subitem).html(submenu.MenuName || submenu.text);
+
+                        $("ul:first", item).append(subitem);
+                    });
+                    mainmenu.append(item);
+
+                });
+
+                //Accordion
+                accordion = $("#mainmenu").ligerAccordion({ height: bodyHeight - 24, speed: null });
+
+
+
+                $("#pageloading").hide();
+            });
+
+            LG.ajax({
+                type: 'AjaxMemberManage',
+                method: 'GetCurrentUser',
+                success: function (user)
+                {
+                    $(".l-topmenu-username").html(user.Title + "，");
+                },
+                error: function ()
+                {
+                    LG.tip('用户信息加载失败');
+                }
+            });
+        });
+    </script>
 </body>
+
 </html>
+
--- www/webapps/views/login.php
+++ www/webapps/views/login.php
@@ -1,1 +1,150 @@
-adsdfdfsd
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="Keywords" content="" />
+    <meta name="Descprition" content="" />
+    <title></title>
+    <script src="<?php echo $media_root.'js/jquery-1.7.2.min.js'; ?>" type="text/javascript"></script>
+    <link href="<?php echo $media_root.'css/ligerui-dialog.css'; ?>" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $media_root.'css/dialog.css'; ?>" rel="stylesheet" type="text/css" />
+    <script src="<?php echo $media_root.'js/base.js'; ?>" type="text/javascript"></script>
+    <script src="<?php echo $media_root.'ligerDialog.js'; ?>" type="text/javascript"></script>
+    <script src="<?php echo $media_root.'js/common.js'; ?>" type="text/javascript"></script>
+    <script src="<?php echo $media_root.'js/LG.js'; ?>" type="text/javascript"></script>
+    <link href="<?php echo $media_root.'css/login.css'; ?>" rel="stylesheet" type="text/css" />
+   
+    <script type="text/javascript">
+        var FromUrl = getQueryStringByName("FromUrl");
+        if (!FromUrl)
+        {
+            FromUrl = encodeURIComponent("index.aspx");
+        }
+        $(function ()
+        {
+            $(".login-text").focus(function ()
+            {
+                $(this).addClass("login-text-focus");
+            }).blur(function ()
+            {
+                $(this).removeClass("login-text-focus");
+            });
+
+            $(document).keydown(function (e)
+            {
+                if (e.keyCode == 13)
+                {
+                    dologin();
+                }
+            });
+
+            $("#btnLogin").click(function ()
+            {
+                dologin();
+            });
+
+
+            function dologin()
+            {
+                var username = $("#txtUsername").val();
+                var password = $("#txtPassword").val();
+                if (username == "")
+                {
+                    alert('账号不能为空!');
+                    $("#txtUsername").focus();
+                    return;
+                }
+                if (password == "")
+                {
+                    alert('密码不能为空!');
+                    $("#txtPassword").focus();
+                    return;
+                }
+                $.ajax({
+                    type: 'post', cache: false, dataType: 'json',
+                    url: 'handler/validate.ashx',
+                    data: [
+                    { name: 'Action', value: 'Login' },
+                    { name: 'username', value: username },
+                    { name: 'password', value: password }
+                    ],
+                    success: function (result)
+                    {
+                        if (!result)
+                        {
+                            alert('登陆失败,账号或密码有误!');
+                            $("#txtUsername").focus();
+                            return;
+                        } else
+                        {
+                            location.href = decodeURIComponent(FromUrl);
+                        }
+                    },
+                    error: function ()
+                    {
+                        alert('发送系统错误,请与系统管理员联系!');
+                    },
+                    beforeSend: function ()
+                    {
+                        $.ligerDialog.waitting("正在登陆中,请稍后...");
+                        $("#btnLogin").attr("disabled", true);
+                    },
+                    complete: function ()
+                    {
+                        $.ligerDialog.closeWaitting();
+                        $("#btnLogin").attr("disabled", false);
+                    }
+                });
+            }
+        });
+    </script>
+</head>
+<body style="padding:10px"> 
+    <div id="login">
+        <div id="loginlogo"></div>
+
+        <div id="loginpanel">
+            <div class="panel-h"></div>
+            <div class="panel-c">
+                <div class="panel-c-l">
+                   
+                    <table cellpadding="0" cellspacing="0">
+                        <tbody>
+                         <tr>
+                            <td align="left" colspan="2"> 
+                             <h3>请使用LigerUI 权限管理系统账号登陆</h3>
+
+                            </td>
+                            </tr> 
+                            <tr>
+                            <td align="right">账号：</td><td align="left"><input type="text" name="loginusername" id="txtUsername" class="login-text" value="test1" /></td>
+                            </tr>
+                            <tr>
+                            <td align="right">密码：</td><td align="left"><input type="password" name="loginpassword" id="txtPassword" class="login-text" value="1" /></td>
+                            </tr> 
+                            <tr>
+
+                            <td align="center" colspan="2">
+                                <input type="submit" id="btnLogin" value="登陆" class="login-btn" />
+                            </td>
+                            </tr> 
+                        </tbody>
+                    </table>
+                </div>
+                <div class="panel-c-r">
+                <p>请从左侧输入登录账号和密码登录</p>
+
+                <p>如果遇到系统问题，请联系网络管理员。</p>
+                <p>如果没有账号，请联系网站管理员。 </p>
+                <p>......</p>
+                </div>
+            </div>
+            <div class="panel-f"></div>
+        </div>
+
+         <div id="logincopyright">Copyright ? 2012 LigerUI </div>
+    </div>
+</body>
+</html>
+
+
