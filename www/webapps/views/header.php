9f03e5b0f957e1e9f00898f6fbbbf8833bce0fb6
robinhuang83@gmail.com <robinhuang83@gmail.com@d3b773b6-36a7-1bf0-39a4-6fa0203e7e18>
2012-05-19 15:35:07 +0000

    update

--- www/webapps/views/header.php
+++ www/webapps/views/header.php
@@ -4,6 +4,7 @@
     <meta http-equiv="content-type" content="text/html; charset=utf8" />
     <title>91消费宝业务员管理系统</title>
     <link type="text/css" rel="stylesheet" href="<?php echo $media_root.'js/ligerui/ligerUI/skins/Aqua/css/ligerui-all.css';?>" />
+    <link type="text/css" rel="stylesheet" href="<?php echo $media_root.'js/ligerui/ligerUI/skins/Gray/css/all.css';?>" />
     <link type="text/css" rel="stylesheet" href="<?php echo $media_root.'css/page.css'?>" />
     <script type="text/javascript" src="<?php echo $media_root.'js/jquery-1.7.2.min.js';?>"></script>
     <script type="text/javascript" src="<?php echo $media_root.'js/ligerui/ligerUI/js/core/base.js';?>"></script>
