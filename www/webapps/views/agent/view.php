eb0a1509e1977a1a7ac36200080d16bc7aeca609
robinhuang83@gmail.com <robinhuang83@gmail.com@d3b773b6-36a7-1bf0-39a4-6fa0203e7e18>
2012-05-20 15:45:51 +0000

    update

--- www/webapps/views/agent/view.php
+++ www/webapps/views/agent/view.php
@@ -1,7 +1,6 @@
 <?php $this->load->view('header');?>
 </head>
 <body>
-    <?php print_r($agent);?>
     <div class="userinfo" style="padding:10px;">
 	<table class="info" border="0" cellspacing="0" cellpadding="0">
 	    <tr>
@@ -19,6 +18,42 @@
 	    <tr>
 		<td>性别</td>
 		<td><?php if (isset($agent['sex'])){if ($agent['sex']==1){echo '男';}elseif ($agent['sex']==2){echo '女';}}?></td>
+	    </tr>
+	    <tr>
+		<td>公司</td>
+		<td><?php echo isset($agent['comp_name'])?$agent['comp_name']:'';?></td>
+	    </tr>
+	    <tr>
+		<td>办公电话</td>
+		<td><?php echo isset($agent['comp_phone'])?$agent['comp_phone']:'';?></td>
+	    </tr>
+	    <tr>
+		<td>手机</td>
+		<td><?php echo isset($agent['mobile_phone'])?$agent['mobile_phone']:'';?></td>
+	    </tr>
+	    <tr>
+		<td>QQ</td>
+		<td><?php echo isset($agent['qq'])?$agent['qq']:'';?></td>
+	    </tr>
+	    <tr>
+		<td>MSN</td>
+		<td><?php echo isset($agent['msg'])?$agent['msg']:'';?></td>
+	    </tr>
+	    <tr>
+		<td>地址</td>
+		<td><?php echo isset($agent['address'])?$agent['address']:'';?></td>
+	    </tr>
+	    <tr>
+		<td>开户银行</td>
+		<td><?php echo isset($agent['bank_name'])?$agent['bank_name']:'';?></td>
+	    </tr>
+	    <tr>
+		<td>开户人名称</td>
+		<td><?php echo isset($agent['bank_user'])?$agent['bank_user']:'';?></td>
+	    </tr>
+	    <tr>
+		<td>银行帐号</td>
+		<td><?php echo isset($agent['bank_no'])?$agent['bank_no']:'';?></td>
 	    </tr>
 	    <tr>
 		<td>创建日期</td>
